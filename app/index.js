d29be483edfe170725c4b907a251df648e449a1a
James Andrews <bdaddyja@gmail.com>
2023-02-28 16:27:17 -0800

    Move comment to right file

--- app/index.js
+++ app/index.js
@@ -1,1 +1,17 @@
 export {default} from '../storybook'; 
+
+/**
+ * file orignally had this
+ */
+
+// -import * as React from 'react';
+// -
+// -import { WorkoutScreen } from './src/screens';
+// -
+// -function App() {
+// -  return (
+// -    <WorkoutScreen />
+// -  )
+// -}
+// -
+// -export default App;
--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,1 @@
 export { request } from "./http";
-
-
-/**
- * file orignally had this
- */
-
-// -import * as React from 'react';
-// -
-// -import { WorkoutScreen } from './src/screens';
-// -
-// -function App() {
-// -  return (
-// -    <WorkoutScreen />
-// -  )
-// -}
-// -
-// -export default App;
