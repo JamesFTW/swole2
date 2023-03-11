2f69e637ec9d444f97959ec4540e7f7530e3bb9a
James Andrews <bdaddyja@gmail.com>
2023-03-10 23:02:26 -0800

    Modified files

---
+++ app/components/actiongroup/ActionGroup.js
@@ -0,0 +1,8 @@
+/*
+    Action groups are a variant button style for grouping
+    of actions that all hold similar priority and visual weight.
+
+    They will initially be used to make the target muscle group pills.
+
+    Being able to add icons would be nice, but will only have text to start off.
+**/
