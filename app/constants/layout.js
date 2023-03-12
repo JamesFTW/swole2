18b19f11b69656358f6dd8d98a2d7a744f68cc7e
James Andrews <bdaddyja@gmail.com>
2023-03-11 23:52:07 -0800

    Add constants

---
+++ app/constants/layout.js
@@ -0,0 +1,26 @@
+export const SPACING_NONE = 0;
+
+export const SPACING_XS_1 = 4;
+export const SPACING_XS_2 = 8;
+export const SPACING_XS_3 = 12;
+export const SPACING_XS_4 = 16;
+
+export const SPACING_S_1 = 20;
+export const SPACING_S_2 = 24;
+export const SPACING_S_3 = 28;
+export const SPACING_S_4 = 32;
+
+export const SPACING_MD_1 = 36;
+export const SPACING_MD_2 = 40;
+export const SPACING_MD_3 = 48;
+export const SPACING_MD_4 = 52;
+
+export const SPACING_LG_1 = 52;
+export const SPACING_LG_2 = 56;
+export const SPACING_LG_3 = 60;
+export const SPACING_LG_4 = 64;
+
+export const SPACING_XL_1 = 68;
+export const SPACING_XL_2 = 72;
+export const SPACING_XL_3 = 76;
+export const SPACING_XL_4 = 80;
