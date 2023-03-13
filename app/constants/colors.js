bcb671799de962cac1232d6d939908996af1e71b
James Andrews <bdaddyja@gmail.com>
2023-03-12 22:14:12 -0700

    Layout and color constants so far

---
+++ app/constants/colors.js
@@ -0,0 +1,15 @@
+export const COLORS = {
+    'primary-color': '#1E2F4E',
+    'light_blue': '#92A3C1',
+    'primary_blue': '#005AFF',
+    'background_blue': '#92A3C1',
+    'subtitle_gray': '#748199',
+    'white': '#FFFFFF',
+    'black': '#000000',
+    'grey': '#605C5C',
+    'black_gradient': ['transparent', 'rgba(0, 0, 0, 0.78)']
+  }
+
+  export const PRIMARY_COLOR = '#1E2F4E';
+  export const SECONDARY_COLOR = '#92A3C1';
+  export const ALERT_COLOR = '#FF2400';
--- app/constants/layout.js
+++ app/constants/layout.js
@@ -1,26 +1,25 @@
 export const SPACING_NONE = 0;
 
-export const SPACING_XS_1 = 4;
-export const SPACING_XS_2 = 8;
-export const SPACING_XS_3 = 12;
-export const SPACING_XS_4 = 16;
+export const SPACING_XS_4 = 4;
+export const SPACING_XS_8 = 8;
+export const SPACING_XS_12 = 12;
+export const SPACING_XS_16 = 16;
 
-export const SPACING_S_1 = 20;
-export const SPACING_S_2 = 24;
-export const SPACING_S_3 = 28;
-export const SPACING_S_4 = 32;
+export const SPACING_S_20 = 20;
+export const SPACING_S_24 = 24;
+export const SPACING_S_28 = 28;
+export const SPACING_S_32 = 32;
 
-export const SPACING_MD_1 = 36;
-export const SPACING_MD_2 = 40;
-export const SPACING_MD_3 = 48;
-export const SPACING_MD_4 = 52;
+export const SPACING_MD_36 = 36;
+export const SPACING_MD_40 = 40;
+export const SPACING_MD_48 = 48;
+export const SPACING_MD_52 = 52;
 
-export const SPACING_LG_1 = 52;
-export const SPACING_LG_2 = 56;
-export const SPACING_LG_3 = 60;
-export const SPACING_LG_4 = 64;
+export const SPACING_LG_56 = 56;
+export const SPACING_LG_60 = 60;
+export const SPACING_LG_64 = 64;
+export const SPACING_LG_68 = 68;
 
-export const SPACING_XL_1 = 68;
-export const SPACING_XL_2 = 72;
-export const SPACING_XL_3 = 76;
-export const SPACING_XL_4 = 80;
+export const SPACING_XL_72 = 72;
+export const SPACING_XL_76 = 76;
+export const SPACING_XL_80 = 80;
