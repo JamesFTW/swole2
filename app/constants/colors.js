d1c218e6c42081bfd748391d2588a6deb403e72d
James Andrews <bdaddyja@gmail.com>
2023-03-17 19:40:28 -0700

    Add theme colors

--- app/constants/colors.js
+++ app/constants/colors.js
@@ -1,15 +1,20 @@
-export const COLORS = {
-    'primary-color': '#1E2F4E',
-    'light_blue': '#92A3C1',
-    'primary_blue': '#005AFF',
-    'background_blue': '#92A3C1',
-    'subtitle_gray': '#748199',
-    'white': '#FFFFFF',
-    'black': '#000000',
-    'grey': '#605C5C',
-    'black_gradient': ['transparent', 'rgba(0, 0, 0, 0.78)']
-  }
 
-  export const PRIMARY_COLOR = '#1E2F4E';
-  export const SECONDARY_COLOR = '#92A3C1';
-  export const ALERT_COLOR = '#FF2400';
+const PRIMARY_COLOR = '#1E2F4E';
+const SECONDARY_COLOR = '#92A3C1';
+const ALERT_COLOR = '#FF2400'
+const WHITE = '#FFFFFF'
+const BLACK = '#000000'
+const GRAY = '#605C5C'
+const SUBTITLE_GRAY = '#748199'
+const BLACK_GRADIENT =  ['transparent', 'rgba(0, 0, 0, 0.78)']
+
+export const COLORS = {
+  PRIMARY_COLOR,
+  SECONDARY_COLOR,
+  ALERT_COLOR,
+  SUBTITLE_GRAY,
+  WHITE,
+  BLACK,
+  GRAY,
+  BLACK_GRADIENT
+}
