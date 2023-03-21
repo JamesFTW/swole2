3613b19a3caa18ee3513c10d657795b4a4b1c3fa
James Andrews <bdaddyja@gmail.com>
2023-03-20 21:59:24 -0400

    Add font sizes and spacing constants

--- app/constants/fonts.js
+++ app/constants/fonts.js
@@ -2,10 +2,18 @@ const SFPRO_REGULAR = 'SFProText-Regular'
 const SFPRO_MEDIUM = 'SFProText-Medium'
 const SFPRO_HEAVY = 'SFProText-Heavy'
 const SFPRO_LIGHT = 'SFProText-Light'
+const SPACING_S = .015
+const SIZE_16 = 16
+const SIZE_20 = 20
+const SIZE_24 = 24
 
 export const FONTS = {
-    SFPRO_REGULAR,
-    SFPRO_MEDIUM,
-    SFPRO_HEAVY,
-    SFPRO_LIGHT
+  SFPRO_REGULAR,
+  SFPRO_MEDIUM,
+  SFPRO_HEAVY,
+  SFPRO_LIGHT,
+  SPACING_S,
+  SIZE_16,
+  SIZE_20,
+  SIZE_24
 }
