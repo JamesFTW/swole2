dba389e643a1266759493d2d8ddc0002bdab5875
James Andrews <bdaddyja@gmail.com>
2023-03-24 16:14:11 -0400

    Update image styles

--- app/components/image/Image.js
+++ app/components/image/Image.js
@@ -1,17 +1,16 @@
 import * as React from 'react'
 import { Image as NativeImage } from 'react-native'
+import styles from './Image.styles'
 
 export function Image({
   src,
-  height,
-  width,
 }) {
   //set up ability to store images to local storage and reterive them if they exist in local storage
   //might also need a way to delete images from local storage after a certain amount of time
   return (
     <NativeImage 
       source={{uri: src}}
-      style={{height, width}}>
+      style={styles.image}>
     </NativeImage>
   )
 }
---
+++ app/components/image/Image.styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+	image: {
+		height: 60,
+        width: 60,
+        borderRadius: 30,
+
+	},
+});
+
+export default styles
