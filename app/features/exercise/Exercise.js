6a589a01ce20f88029fbc179ad1e2cf09ba7c4f7
James Andrews <bdaddyja@gmail.com>
2023-03-24 22:42:10 -0700

    Add Exercise component

--- app/features/exercise/Exercise.js
+++ app/features/exercise/Exercise.js
@@ -1,3 +1,25 @@
-export function Exercise() {
 
+import * as React from 'react'
+import { Text } from 'react-native'
+import { Card, Image } from '../../components'
+import { FlexContainer } from '../../layout'
+import styles from './Exercise.styles'
+
+export function Exercise({
+  exerciseTitle = "Bench Press",
+}) {
+  return (
+    <Card>
+      <FlexContainer direction='row'>
+        <Image/>
+        <FlexContainer direction='column'>
+          <Text style={styles.exercise_title}>{ exerciseTitle }</Text>
+          <>
+          <Text style={styles.exercise_subtitle}>Bruh</Text>
+          <Text style={styles.exercise_subtitle}>Bruh</Text>
+          </>
+        </FlexContainer>
+      </FlexContainer>
+    </Card>
+  )
 }
---
+++ app/features/exercise/Exercise.styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+import { FONTS, COLORS, LAYOUT } from '../../constants'
+
+const styles = StyleSheet.create({
+	exercise_title: {
+		fontFamily: FONTS.SFPRO_REGULAR,
+		fontSize: FONTS.SIZE_20,
+		color: COLORS.PRIMARY_COLOR,
+		letterSpacing: FONTS.SPACING_S,
+		marginLeft: LAYOUT.SPACING_S_20,
+	},
+	exercise_subtitle: {
+		fontFamily: FONTS.SFPRO_REGULAR,
+		fontSize: FONTS.SIZE_16,
+		letterSpacing: FONTS.SPACING_S,
+		color: COLORS.SUBTITLE_GRAY,
+		marginLeft: LAYOUT.SPACING_XS_16,
+	}
+})
+
+export default styles
