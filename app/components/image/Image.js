13852ad4ee9794a964550f9e3331f607eac6f997
James Andrews <bdaddyja@gmail.com>
2023-03-25 21:58:30 -0700

    Add helper layout props

--- app/components/image/Image.js
+++ app/components/image/Image.js
@@ -5,13 +5,22 @@ const placeHolder = 'https://cdn.shopify.com/s/files/1/1876/4703/articles/shutte
 
 export function Image({
   src = placeHolder,
+  marginLeft,
+  marginRight,
+  marginTop,
+  marginBottom,
 }) {
   //set up ability to store images to local storage and reterive them if they exist in local storage
   //might also need a way to delete images from local storage after a certain amount of time
   return (
     <NativeImage 
       source={{uri: src}}
-      style={styles.image}>
+      style={[styles.image, {
+        marginTop,
+        marginLeft,
+        marginRight,
+        marginBottom,
+      }]}>
     </NativeImage>
   )
 }
