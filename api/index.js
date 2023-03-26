5016b1ea6706f996b03b8141fc8c25445bcfe471
James Andrews <bdaddyja@gmail.com>
2023-03-26 14:22:51 -0700

    Hello world vercel

--- api/index.js
+++ api/index.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// Add Express
+const express = require("express");
+// Initialize Express
+const app = express();
+// Create GET request
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+// Initialize server
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+module.exports = app;
