2339dc0e25664b4ec187d9737f4620eed598439d
James Andrews <bdaddyja@gmail.com>
2023-04-01 14:01:02 -0700

    clean up cardgroup

--- app/components/cardgroup/stories/CardGroup.stories.js
+++ app/components/cardgroup/stories/CardGroup.stories.js
@@ -1,27 +1,10 @@
 import * as React from 'react'
-import {storiesOf, addParameters, addDecorator} from '@storybook/react-native';
-import {action} from '@storybook/addon-actions';
-import {CardGroup} from '../CardGroup';
-import {StyleSheet, View, Text, Image} from 'react-native';
+import { storiesOf } from '@storybook/react-native';
+import { action } from '@storybook/addon-actions';
+import { CardGroup } from '../CardGroup';
 import { withBackgrounds } from '@storybook/addon-ondevice-backgrounds';
 import { withKnobs } from '@storybook/addon-ondevice-knobs';
-import { Card } from '../../../components'
 import { WorkoutExercise } from '../../../features/workoutexercise/WorkoutExercise';
-
-const placeHolder = 'https://cdn.shopify.com/s/files/1/1876/4703/articles/shutterstock_1079398565_2560x.jpg?v=1591108584'
-
-const CenterDecorator = storyFn => (
-    <View style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-       }}>
-      {
-				storyFn()
-			}
-    </View>
-  );
-
 
 export default storiesOf('CardGroup', module)
 	.addDecorator(withBackgrounds)
