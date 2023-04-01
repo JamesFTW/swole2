cbbe349cdf56e507584f2188eacba6c74c2ccde1
James Andrews <bdaddyja@gmail.com>
2023-03-31 18:47:24 -0700

    Clean up card stories

--- app/components/card/stories/Card.stories.js
+++ app/components/card/stories/Card.stories.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import {storiesOf, addParameters, addDecorator} from '@storybook/react-native';
-import {action} from '@storybook/addon-actions';
-import {Card} from '../Card';
-import {StyleSheet, View, Text, Image} from 'react-native';
+import { storiesOf } from '@storybook/react-native';
+import { action} from '@storybook/addon-actions';
+import { Card } from '../Card';
+import { View } from 'react-native';
 import { withBackgrounds } from '@storybook/addon-ondevice-backgrounds';
 import { withKnobs } from '@storybook/addon-ondevice-knobs';
-import styles from '../../../styles/image.styles'
-
-const placeHolder = 'https://cdn.shopify.com/s/files/1/1876/4703/articles/shutterstock_1079398565_2560x.jpg?v=1591108584'
 
 //react aria has usebutton hook for props
 
