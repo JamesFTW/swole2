3b8cf383532d5169470ee91a67d8ce95dc29dfdf
James Andrews <bdaddyja@gmail.com>
2023-03-31 18:43:40 -0700

    Add action component with stories

--- app/components/action/Action.js
+++ app/components/action/Action.js
@@ -1,22 +1,36 @@
 import * as React from 'react'
-import { Text, Pressable } from 'react-native'
+import { Text, Pressable, View } from 'react-native'
 import styles from './Action.styles'
 
-
-/**
- * 
- * TODO: Add implementation
- * 
- */
+const ACTION_BUTTON_COLORS = {
+  'CHEST': '#0F2560',
+  'BACK': '#5F4A8B',
+  'SHOULDERS': '#00203F',
+  'LEGS': '#01539D'
+}
+const ACTION_TEXT_COLORS = {
+  'CHEST': '#F1D702',
+  'BACK': '#E69A8D',
+  'SHOULDERS': '#ADF0D1',
+  'LEGS': '#FFD662'
+}
 
 export function Action({
   onPress,
-  children
+  actionTitle
 }) {
+  const actionTitleToUpperCase = actionTitle.toUpperCase()
 
   return (
-    <Pressable style={styles.container} onPress={onPress}>
-      <Text style={styles.text}>Chest</Text>
+    <Pressable 
+      style={[styles.action_container, {'backgroundColor': ACTION_BUTTON_COLORS[actionTitleToUpperCase]}]} 
+      onPress={onPress}>
+      <View style={[styles.action_inside]}>
+        <Text
+          style={[styles.action_title, {'color': ACTION_TEXT_COLORS[actionTitleToUpperCase]}]}>
+            {actionTitleToUpperCase}
+        </Text>
+      </View>
     </Pressable>
   )
 }
