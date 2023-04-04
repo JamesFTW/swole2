10149189341461921354bd0613e3fa88266f3ea0
James Andrews <bdaddyja@gmail.com>
2023-04-03 18:43:50 -0700

    use spaces

--- storybook/CenterDecorator.js
+++ storybook/CenterDecorator.js
@@ -2,14 +2,14 @@ import React from "react";
 import { View } from 'react-native'
 
 export const CenterDecorator = storyFn => (
-	<View style={{
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-		alignSelf: 'center'
-	}}>
-		{
-			storyFn()
-		}
-	</View>
+  <View style={{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center'
+  }}>
+    {
+      storyFn()
+    }
+  </View>
 );
