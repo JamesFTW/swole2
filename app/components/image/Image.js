64c0e8cae666ddd38da390542a60f618ad3912d4
James Andrews <bdaddyja@gmail.com>
2023-04-03 19:04:27 -0700

    Add image defaults

--- app/components/image/Image.js
+++ app/components/image/Image.js
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { Image as NativeImage } from 'react-native'
-import styles from './Image.styles'
 const placeHolder = 'https://cdn.shopify.com/s/files/1/1876/4703/articles/shutterstock_1079398565_2560x.jpg?v=1591108584'
 
 export function Image({
@@ -9,18 +8,24 @@ export function Image({
   marginRight,
   marginTop,
   marginBottom,
+  height = 60,
+  width = 60,
+  borderRadius = 30,
 }) {
   //set up ability to store images to local storage and reterive them if they exist in local storage
   //might also need a way to delete images from local storage after a certain amount of time
   return (
     <NativeImage 
       source={{uri: src}}
-      style={[styles.image, {
+      style={{
         marginTop,
         marginLeft,
         marginRight,
         marginBottom,
-      }]}>
+        height,
+        width,
+        borderRadius
+      }}>
     </NativeImage>
   )
 }
