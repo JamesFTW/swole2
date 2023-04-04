68443a5b97861264f59ce1dd53b2e3411b60c5a7
James Andrews <bdaddyja@gmail.com>
2023-04-03 19:05:20 -0700

    Add stories

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -10,6 +10,7 @@ function loadStories() {
   require('../app/components/card/stories/Card.stories');
   require('../app/components/cardgroup/stories/CardGroup.stories');
   require('../app/components/image/stories/Image.stories');
+  require('../app/features/exercise/stories/Exercise.stories');
   require('../app/features/workoutexercise/stories/WorkoutExercise.stories');
   require('../app/features/workoutexercise/stories/WorkoutExerciseInfo.stories');
 }
@@ -21,6 +22,7 @@ const stories = [
   '../app/components/card/stories/Card.stories',
   '../app/components/cardgroup/stories/CardGroup.stories',
   '../app/components/image/stories/Image.stories',
+  '../app/features/exercise/stories/Exercise.stories',
   '../app/features/workoutexercise/stories/WorkoutExercise.stories',
   '../app/features/workoutexercise/stories/WorkoutExerciseInfo.stories',
 ];
