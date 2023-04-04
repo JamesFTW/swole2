a3b100d81b349b68689eecee4fd9e4e38aa5c76a
James Andrews <bdaddyja@gmail.com>
2023-04-03 19:08:20 -0700

    Add Exercise component

---
+++ app/features/exercise/Exercise.js
@@ -0,0 +1,32 @@
+import * as React from 'react'
+import { Text } from 'react-native'
+import { Card, Image, ActionGroup } from '../../components'
+import { LAYOUT } from '../../constants'
+import { FlexContainer } from '../../layout'
+import styles from './Exercise.styles'
+
+export function Exercise({
+  exerciseTitle,
+  primaryMuscleGroup,
+  exerciseImage
+}) {
+  return (
+    <Card cardHeight={LAYOUT.SPACING_XL_76}>
+      <FlexContainer direction={LAYOUT.FLEX_ROW}>
+        <Image
+          src={exerciseImage}
+          height={50}
+          width={50}
+          borderRadius={25}
+          marginTop={LAYOUT.SPACING_NUDGE_S}
+        />
+        <FlexContainer direction={LAYOUT.FLEX_COLUMN}>
+          <FlexContainer>
+            <Text style={styles.exercise_title}>{ exerciseTitle }</Text>
+          </FlexContainer>
+          <ActionGroup marginLeft={LAYOUT.SPACING_S_20} marginTop={LAYOUT.SPACING_XS_8} titles={primaryMuscleGroup} />
+        </FlexContainer>
+      </FlexContainer>
+    </Card>
+  )
+}
---
+++ app/features/exercise/Exercise.styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+import { FONTS, COLORS, LAYOUT } from '../../constants'
+
+const styles = StyleSheet.create({
+	exercise_title: {
+		fontFamily: FONTS.SFPRO_REGULAR,
+		fontSize: FONTS.SIZE_20,
+		color: COLORS.PRIMARY_COLOR,
+		letterSpacing: FONTS.SPACING_S,
+		marginLeft: LAYOUT.SPACING_S_20
+	},
+	exercise_subtitle: {
+		fontFamily: FONTS.SFPRO_REGULAR,
+		fontSize: FONTS.SIZE_16,
+		letterSpacing: FONTS.SPACING_S,
+		color: COLORS.SUBTITLE_GRAY,
+	}
+})
+
+export default styles
