d59dcc44f517978803284c668e4508b3f4428db1
James Andrews <bdaddyja@gmail.com>
2023-04-03 19:09:38 -0700

    Use spaces

--- app/features/exercise/Exercise.styles.js
+++ app/features/exercise/Exercise.styles.js
@@ -2,19 +2,19 @@ import { StyleSheet } from 'react-native';
 import { FONTS, COLORS, LAYOUT } from '../../constants'
 
 const styles = StyleSheet.create({
-	exercise_title: {
-		fontFamily: FONTS.SFPRO_REGULAR,
-		fontSize: FONTS.SIZE_20,
-		color: COLORS.PRIMARY_COLOR,
-		letterSpacing: FONTS.SPACING_S,
-		marginLeft: LAYOUT.SPACING_S_20
-	},
-	exercise_subtitle: {
-		fontFamily: FONTS.SFPRO_REGULAR,
-		fontSize: FONTS.SIZE_16,
-		letterSpacing: FONTS.SPACING_S,
-		color: COLORS.SUBTITLE_GRAY,
-	}
+  exercise_title: {
+    fontFamily: FONTS.SFPRO_REGULAR,
+    fontSize: FONTS.SIZE_20,
+    color: COLORS.PRIMARY_COLOR,
+    letterSpacing: FONTS.SPACING_S,
+    marginLeft: LAYOUT.SPACING_S_20
+  },
+  exercise_subtitle: {
+    fontFamily: FONTS.SFPRO_REGULAR,
+    fontSize: FONTS.SIZE_16,
+    letterSpacing: FONTS.SPACING_S,
+    color: COLORS.SUBTITLE_GRAY,
+  }
 })
 
 export default styles
