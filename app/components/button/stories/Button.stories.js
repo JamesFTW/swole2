3a80f423735b964589bd1e07b03256b9e7c978c8
James Andrews <bdaddyja@gmail.com>
2023-05-15 15:41:31 -0700

    formatting

--- app/components/button/stories/Button.stories.js
+++ app/components/button/stories/Button.stories.js
@@ -9,24 +9,24 @@ import EmailIcon from '../../../assets/icons/email.svg'
 //react aria has usebutton hook for props
 
 export default storiesOf('Button', module)
-    .add("outline", () => (
-        <Button outline 
-            icon={<EmailIcon style={styles.email_icon} />} 
-            title="Sign Up with Email"
-            onPress={action("tapped-default")}
-        />
-    ))
-    .add("default", () => (
-        <Button
-            icon={<EmailIcon style={styles.email_icon} />} 
-            title="Submit"
-            onPress={action("tapped-default")}
-        />
-    ))
+  .add("outline", () => (
+    <Button outline
+      icon={<EmailIcon style={styles.email_icon} />}
+      title="Sign Up with Email"
+      onPress={action("tapped-default")}
+    />
+  ))
+  .add("default", () => (
+    <Button
+      icon={<EmailIcon style={styles.email_icon} />}
+      title="Submit"
+      onPress={action("tapped-default")}
+    />
+  ))
 
 
 const styles = StyleSheet.create({
-    email_icon: {
-        marginLeft: LAYOUT.SPACING_XS_16
-    }
+  email_icon: {
+    marginLeft: LAYOUT.SPACING_XS_16
+  }
 });
