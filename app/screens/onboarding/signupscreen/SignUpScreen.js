395a02a3d387c177b0ef7f3baa2b93348dc93b4d
James Andrews <bdaddyja@gmail.com>
2023-05-20 14:21:47 -0700

    Add signupscreen

--- app/screens/onboarding/signupscreen/SignUpScreen.js
+++ app/screens/onboarding/signupscreen/SignUpScreen.js
@@ -1,9 +1,44 @@
 import React from 'react'
-import { ImageBackground, View, Text, StatusBar } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
+import { LAYOUT } from '../../../constants'
+import { Button, FormInput, TextButton } from '../../../components'
+import styles from './SignUpScreen.styles'
 
+const CONSTANTS = {
+  EMAIL_PLACEHOLDER: 'iamveryswole@swole.com',
+  PASSWORD_REQUIREMENT_TEXT: 'Password must contain at least 8 characters',
+  PASSWORD_PLACEHOLDER: '*******',
+  SIGN_UP_WITH_EMAIL: 'Sign up with email'
+}
 
-export function SignUpScreen() {
+export function SignUpScreen({navigation}) {
   return (
-    <Text>SIGN UP SCREEN</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.signUpScreenContent}>
+        <TextButton 
+          onPress={() => navigation.goBack()} 
+          marginTop={LAYOUT.SPACING_S_28} 
+          style={styles.backButton}>
+            Back
+        </TextButton>
+        <Text style={styles.title}>{CONSTANTS.SIGN_UP_WITH_EMAIL}</Text>
+        <View style={styles.content}>
+          <FormInput 
+            textContentType={'emailAddress'} 
+            placeholder={CONSTANTS.EMAIL_PLACEHOLDER}
+            title="Email"
+          />
+          <FormInput 
+            secureTextEntry 
+            textContentType={'newPassword'} 
+            placeholder={CONSTANTS.PASSWORD_PLACEHOLDER}
+            marginTop={LAYOUT.SPACING_S_20} 
+            title="Password"
+          />
+          <Text style={styles.passwordRequirementText}>{CONSTANTS.PASSWORD_REQUIREMENT_TEXT}</Text>
+          <Button title="Submit"/>
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
