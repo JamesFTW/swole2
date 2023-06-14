fbf7fa4111015caed560a4ee380fce255ab4ff9c
James Andrews <bdaddyja@gmail.com>
2023-06-13 20:47:12 -0700

    Update getObjData method

--- app/services/asyncstorage.js
+++ app/services/asyncstorage.js
@@ -1,8 +1,17 @@
 import  RNAsyncStorage from '@react-native-async-storage/async-storage'
 
+export const ASYNC_STORE_CONSTANTS = {
+  USER_DATA: '@user_data',
+  USER_SESSION_COOKIE: '@user_session_cookie'
+}
+
+/**
+ * Might have to return a promise for storeObjData and removeValue
+ * When that comes up fix it
+ */
 export class AsyncStorage {
   constructor(data) {
-    this.data = data
+    this.data = data ?? null
   }
 
   storeObjData = async (storage_key) => {
@@ -10,25 +19,26 @@ export class AsyncStorage {
       const jsonValue = JSON.stringify(this.data)
       await RNAsyncStorage.setItem(storage_key, jsonValue)
     } catch (error) {
-      console.log(error)
       throw new Error(error)
     }
   }
 
-  getObjData = async (storage_key) => {
-    try {
-      const jsonValue = await RNAsyncStorage.getItem(storage_key)
-      return jsonValue != null ? JSON.parse(jsonValue) : null
-    } catch(e) {
-      console.log(e)
-    }
+  getObjData = (storage_key) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const jsonValue = await RNAsyncStorage.getItem(storage_key)
+        resolve(jsonValue != null ? JSON.parse(jsonValue) : null)
+      } catch(error) {
+        reject(new Error(error))
+      }
+    })
   }
 
   removeValue = async (storage_key) => {
     try {
       await AsyncStorage.removeItem(storage_key)
     } catch(error) {
-     console.log(error)
+      throw new Error(error)
     }
   
     console.log('Done.')
