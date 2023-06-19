81c861f7c6e86a352a00785df7257525b957d9c7
James Andrews <bdaddyja@gmail.com>
2023-06-19 12:40:40 -0700

    Remove comments

--- app/assets/icons/checkmark/Checkmark.js
+++ app/assets/icons/checkmark/Checkmark.js
@@ -1,6 +1,4 @@
 import React from "react"
-// import { StyleSheet } from 'react-native'
-// import { LAYOUT } from '../../../constants'
 import CheckmarkSVN from '../../../assets/icons/checkmark/checkmark.svg'
 
 export const Checkmark = () => {
@@ -8,9 +6,3 @@ export const Checkmark = () => {
     <CheckmarkSVN />
   )
 }
-
-// const styles = StyleSheet.create({
-//   email_icon: {
-//     marginLeft: LAYOUT.SPACING_XS_16
-//   }
-// });
