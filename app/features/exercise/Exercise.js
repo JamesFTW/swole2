8bdb026642e1196bcd82a2f3ac43d154103ff91f
James Andrews <bdaddyja@gmail.com>
2023-07-05 14:05:05 -0700

    Add onPress prop + clean up

--- app/features/exercise/Exercise.js
+++ app/features/exercise/Exercise.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
 import { Card, Image, ActionGroup } from '../../components'
 import { LAYOUT } from '../../constants'
 import { FlexContainer } from '../../layout'
@@ -8,25 +8,33 @@ import styles from './Exercise.styles'
 export function Exercise({
   exerciseTitle,
   primaryMuscleGroup,
-  exerciseImage
+  exerciseImage,
+  marginBottom,
+  onPress
 }) {
   return (
-    <Card borderRadius cardHeight={LAYOUT.SPACING_XL_76}>
-      <FlexContainer direction={LAYOUT.FLEX_ROW}>
-        <Image
-          src={exerciseImage}
-          height={50}
-          width={50}
-          borderRadius={25}
-          marginTop={LAYOUT.SPACING_NUDGE_S}
-        />
-        <FlexContainer direction={LAYOUT.FLEX_COLUMN}>
-          <FlexContainer>
-            <Text style={styles.exercise_title}>{ exerciseTitle }</Text>
+    <View style={{marginBottom: marginBottom}}>
+      <Card onPress={onPress} borderRadius cardHeight={LAYOUT.SPACING_XL_76}>
+        <FlexContainer direction={LAYOUT.FLEX_ROW}>
+          <Image
+            src={exerciseImage}
+            height={50}
+            width={50}
+            borderRadius={25}
+            marginTop={LAYOUT.SPACING_NUDGE_S}
+          />
+          <FlexContainer direction={LAYOUT.FLEX_COLUMN}>
+            <FlexContainer>
+              <Text style={styles.exercise_title}>{ exerciseTitle }</Text>
+            </FlexContainer>
+            <ActionGroup
+              marginLeft={LAYOUT.SPACING_XS_16}
+              marginTop={LAYOUT.SPACING_XS_8}
+              actionTitles={primaryMuscleGroup}
+            />
           </FlexContainer>
-          <ActionGroup marginLeft={LAYOUT.SPACING_XS_16} marginTop={LAYOUT.SPACING_XS_8} actionTitles={primaryMuscleGroup} />
         </FlexContainer>
-      </FlexContainer>
-    </Card>
+      </Card>
+    </View>
   )
 }
