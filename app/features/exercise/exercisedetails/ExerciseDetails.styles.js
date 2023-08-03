943af0a65525fd650b8354310e2ba51f5ea58269
James Andrews <bdaddyja@gmail.com>
2023-08-03 13:17:21 -0700

    Add user exercise details and last attempts

--- app/features/exercise/exercisedetails/ExerciseDetails.styles.js
+++ app/features/exercise/exercisedetails/ExerciseDetails.styles.js
@@ -14,16 +14,16 @@ const styles = StyleSheet.create({
     fontFamily: FONTS.SFPRO_HEAVY,
     fontSize: FONTS.SIZE_20,
     letterSpacing: FONTS.SPACING_S,
-    marginLeft: 16,
+    marginLeft: LAYOUT.SPACING_XS_16,
   },
   exercise_description: {
     color: COLORS.PRIMARY_COLOR,
     fontFamily: FONTS.SFPRO_REGULAR,
     fontSize: FONTS.SIZE_14,
     letterSpacing: .25,
-    marginLeft: 20,
-    marginRight: 15,
-    lineHeight: 21,
+    marginLeft: LAYOUT.SPACING_S_20,
+    marginRight: LAYOUT.SPACING_XS_16,
+    lineHeight: LAYOUT.SPACING_S_20,
     marginTop: 28
   },
   target_muscle: {
@@ -38,8 +38,8 @@ const styles = StyleSheet.create({
     fontFamily: FONTS.SFPRO_REGULAR,
     fontSize: FONTS.SIZE_20,
     letterSpacing: FONTS.SPACING_S,
-    marginLeft: 16,
-    marginTop: 28
+    marginLeft: LAYOUT.SPACING_XS_16,
+    marginTop: LAYOUT.SPACING_S_28
   },
   total_weight: {
     color: COLORS.PRIMARY_COLOR,
@@ -51,8 +51,8 @@ const styles = StyleSheet.create({
   total_weight_container: {
     position: 'absolute',
     right: 0,
-    marginRight: 16,
-    marginTop: 21
+    marginRight: LAYOUT.SPACING_XS_16,
+    marginTop: LAYOUT.SPACING_S_20
   },
   total_weight_subtitle: {
     color: COLORS.SECONDARY_COLOR,
@@ -60,6 +60,25 @@ const styles = StyleSheet.create({
     fontSize: FONTS.SIZE_14,
     letterSpacing: -.02,
     marginTop: 4
+  },
+  exercise_last_attempt_date: {
+    marginLeft: LAYOUT.SPACING_XS_16,
+    fontFamily: FONTS.SFPRO_REGULAR,
+    fontSize: FONTS.SIZE_16,
+    letterSpacing: .05
+  },
+  exercise_last_attempt_rep: {
+    marginRight: LAYOUT.SPACING_XS_16,
+    fontFamily: FONTS.SFPRO_REGULAR,
+    fontSize: FONTS.SIZE_16,
+    letterSpacing: .05
+  },
+  exercise_first_attempt: {
+    marginTop: LAYOUT.SPACING_S_20,
+    marginLeft: LAYOUT.SPACING_XS_12,
+    fontFamily: FONTS.SFPRO_REGULAR,
+    fontSize: FONTS.SIZE_16,
+    letterSpacing: .05
   }
 })
 
