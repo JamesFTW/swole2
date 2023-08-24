8c8b2d93888adcc077e351f19f4ef61698c79921
James Andrews <bdaddyja@gmail.com>
2023-08-24 16:17:18 -0700

    Use spaces

--- app/features/workoutexercise/WorkoutExercise.styles.js
+++ app/features/workoutexercise/WorkoutExercise.styles.js
@@ -2,26 +2,26 @@ import { StyleSheet } from 'react-native';
 import { FONTS, COLORS, LAYOUT } from '../../constants'
 
 const styles = StyleSheet.create({
-	exercise_title: {
-		fontFamily: FONTS.SFPRO_REGULAR,
-		fontSize: FONTS.SIZE_16,
-		color: COLORS.PRIMARY_COLOR,
-		letterSpacing: FONTS.SPACING_S,
-		marginLeft: LAYOUT.SPACING_XS_16,
-		marginTop: 5
-	},
-	exercise_subtitle: {
-		fontFamily: FONTS.SFPRO_REGULAR,
-		fontSize: FONTS.SIZE_16,
-		letterSpacing: FONTS.SPACING_S,
-		color: COLORS.SUBTITLE_GRAY,
-	},
-	sets_header: {
-		fontFamily: FONTS.SFPRO_MEDIUM,
-		fontSize: FONTS.SIZE_14,
-		color: COLORS.PRIMARY_COLOR,
-		letterSpacing: .01
-	}
+  exercise_title: {
+    fontFamily: FONTS.SFPRO_REGULAR,
+    fontSize: FONTS.SIZE_16,
+    color: COLORS.PRIMARY_COLOR,
+    letterSpacing: FONTS.SPACING_S,
+    marginLeft: LAYOUT.SPACING_XS_16,
+    marginTop: 5
+  },
+  exercise_subtitle: {
+    fontFamily: FONTS.SFPRO_REGULAR,
+    fontSize: FONTS.SIZE_16,
+    letterSpacing: FONTS.SPACING_S,
+    color: COLORS.SUBTITLE_GRAY,
+  },
+  sets_header: {
+    fontFamily: FONTS.SFPRO_MEDIUM,
+    fontSize: FONTS.SIZE_14,
+    color: COLORS.PRIMARY_COLOR,
+    letterSpacing: .01
+  }
 })
 
 export default styles
