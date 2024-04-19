978380012e32e16f423efe765d103e41fd872d8c
Jdombrowski <jdombrowski416@gmail.com>
2024-04-19 10:36:36 -0700

    Reintroduce Onboarding to get login to work to get api creds for other calls

--- app/index.js
+++ app/index.js
@@ -25,10 +25,7 @@ function App() {
     <NavigationContainer>
       <QueryClientProvider client={queryClient}>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-          {/* <Stack.Screen
-              name="Onboarding"
-              component={OnBoardingStack}
-            /> */}
+          <Stack.Screen name="Onboarding" component={OnBoardingStack} />
           <Stack.Screen name={MainNavigationRoute} component={MainNavigation} />
         </Stack.Navigator>
       </QueryClientProvider>
